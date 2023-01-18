c187c7df12c45c8261c6788d43c1efa7976e01f6
Lyciih <gnehciyil@gmail.com>
2023-01-18 22:49:27 +0800

    setting for vscode debug

---
+++ debug/asm/context_switch.s
@@ -0,0 +1,104 @@
+.macro reg_save base
+	sw ra, 0(\base)
+	sw sp, 4(\base)
+	sw gp, 8(\base)
+	sw tp, 12(\base)
+	sw t0, 16(\base)
+	sw t1, 20(\base)
+	sw t2, 24(\base)
+	sw s0, 28(\base)
+	sw s1, 32(\base)
+	sw a0, 36(\base)
+	sw a1, 40(\base)
+	sw a2, 44(\base)
+	sw a3, 48(\base)
+	sw a4, 52(\base)
+	sw a5, 56(\base)
+	sw a6, 60(\base)
+	sw a7, 64(\base)
+	sw s2, 68(\base)
+	sw s3, 72(\base)
+	sw s4, 76(\base)
+	sw s5, 80(\base)
+	sw s6, 84(\base)
+	sw s7, 88(\base)
+	sw s8, 92(\base)
+	sw s9, 96(\base)
+	sw s10, 100(\base)
+	sw s11, 104(\base)
+	sw t3, 108(\base)
+	sw t4, 112(\base)
+	sw t5, 116(\base)
+.endm
+
+.macro reg_restore base
+	lw ra, 0(\base)
+	lw sp, 4(\base)
+	lw gp, 8(\base)
+	lw tp, 12(\base)
+	lw t0, 16(\base)
+	lw t1, 20(\base)
+	lw t2, 24(\base)
+	lw s0, 28(\base)
+	lw s1, 32(\base)
+	lw a0, 36(\base)
+	lw a1, 40(\base)
+	lw a2, 44(\base)
+	lw a3, 48(\base)
+	lw a4, 52(\base)
+	lw a5, 56(\base)
+	lw a6, 60(\base)
+	lw a7, 64(\base)
+	lw s2, 68(\base)
+	lw s3, 72(\base)
+	lw s4, 76(\base)
+	lw s5, 80(\base)
+	lw s6, 84(\base)
+	lw s7, 88(\base)
+	lw s8, 92(\base)
+	lw s9, 96(\base)
+	lw s10, 100(\base)
+	lw s11, 104(\base)
+	lw t3, 108(\base)
+	lw t4, 112(\base)
+	lw t5, 116(\base)
+	lw t6, 120(\base)
+.endm
+
+
+
+
+.text
+
+#------------------------從kernel跳到task的程式----------------------------------
+.globl switch_to		
+.align 4
+switch_to:						# a0 代表第一個參數，是指向第一個task的context的指針
+	csrw mscratch, a0
+	mv	t6, a0					#把 a0 寫入t6，用來當作載入第一個task的context的基址			
+	reg_restore t6				#利用 t6 當基址載入第一個task的context
+    ret							#用ra覆蓋PC，跳到task的起始位置開始執行
+
+
+
+
+#--------------------------從task跳到task的程式----------------------------------
+.globl 	sys_switch		
+.align 4
+sys_switch:
+	csrrw	t6, mscratch, t6	#將 t6 與 mscratch 做交換，暫時存放t6的值
+	mv	t6, a0					#把 a0 參數寫入t6
+	reg_save t6					#利用 t6 中的地址儲當前的暫存器內容到自己的context
+	mv	t5, t6					#因為 t5 已經存過了，複寫沒關係，把 t6 中的地址寫到t5中
+	csrr	t6, mscratch		#把 t6 跟 mscratch 交換前的值換回來
+	sw	t6, 120(t5)				#利用 t5 中的地址儲存換回來的 t6 到自己context的 t6 位置
+
+								#以上完成存context的動作，下一階段開始載入目標task的context
+
+								# a1 代表第二個參數，是指向下一個task的context的指針
+	mv	t6, a1					#把 a1 寫入t6	
+	reg_restore t6				#用 t6 中的地址載入下一個task的context
+    ret							#用ra覆蓋PC，跳到下一個task的起始位置開始執行
+.end
+
+
---
+++ debug/kernel/schedule.c
@@ -0,0 +1,251 @@
+#include"os.h"
+
+
+int process_ID = 0;
+
+dllNode_t * pcb_list;
+dllNode_t * current_task = NULL;
+pcb_t * manage;
+struct context trap_context;
+
+void software_interrupt()
+{
+    int id = r_mhartid();
+    *(uint32_t *)CLINT_MSIP(id) = 1;
+}
+
+void task_delay(volatile int count)
+{
+	count *= 50000;
+	while (count--);
+}
+
+void task_exit()
+{
+    ((pcb_t *)current_task)->finish = 1;
+    showPCB();
+    switch_to( &(manage->context), manage->pasue_address);
+}
+
+
+
+
+void task_wait()
+{
+    ((pcb_t *)current_task)->waiting = 1;
+    ((pcb_t *)current_task)->finish = 0;
+    software_interrupt();
+}
+
+//第1個task
+void user_task1(char* santams)
+{    
+    printf("Task 1: Running...|%s\n", santams);
+    uart_puts("                  |...\n");
+    uart_puts("                     |...\n");
+    uart_puts("                        |...\n");
+    asm volatile("wfi");
+    task_exit();
+}
+//第2個task
+void user_task2(char* santams)
+{
+    printf("Task 2: Running............|.........%s\n", santams);
+    uart_puts("                        ...|\n");
+    uart_puts("                     ...|\n");
+    uart_puts("                  ...|\n");
+    trap_test();
+    asm volatile("wfi");
+    task_exit();
+}
+
+//第3個task
+void user_task3(char* santams)
+{
+    printf("                    Task 3: Running...|%s\n", santams);
+    uart_puts("                                   |...\n");
+    uart_puts("                                      |...\n");
+    uart_puts("                                         |...\n");
+    create_alarm(20);
+    task_wait();
+    asm volatile("wfi");
+    task_exit();
+}
+//第4個task
+void user_task4(char* santams)
+{
+    printf("                    Task 4: Running............|.........%s\n", santams);
+    uart_puts("                                         ...|\n");
+    uart_puts("                                      ...|\n");
+    uart_puts("                                   ...|\n");
+    asm volatile("wfi");
+    task_exit();
+}
+
+//第5個task
+void user_task5(char* santams)
+{
+    printf("                                        Task 5: Running...|%s\n", santams);
+    uart_puts("                                                       |...\n");
+    uart_puts("                                                          |...\n");
+    uart_puts("                                                             |...\n");
+    asm volatile("wfi");
+    task_exit();
+}
+//第6個task
+void user_task6(char* santams)
+{
+    printf("                                        Task 6: Running............|.........%s\n", santams);
+    uart_puts("                                                             ...|\n");
+    uart_puts("                                                          ...|\n");
+    uart_puts("                                                       ...|\n");
+    create_alarm(10);
+    task_wait();
+    asm volatile("wfi");
+    task_exit();
+}
+
+//第7個task
+void user_task7(char* santams)
+{
+    printf("                                                            Task 7: Running...|%s\n", santams);
+    uart_puts("                                                                           |...\n");
+    uart_puts("                                                                              |...\n");
+    uart_puts("                                                                                 |...\n");
+    asm volatile("wfi");
+    task_exit();
+}
+//第8個task
+void user_task8(char* santams)
+{
+    printf("                                                            Task 8: Running............|.........%s\n", santams);
+    uart_puts("                                                                                 ...|\n");
+    uart_puts("                                                                              ...|\n");
+    uart_puts("                                                                           ...|\n");
+    create_alarm(40);
+    task_wait();  
+    asm volatile("wfi");
+    task_exit();
+}
+
+//第9個task
+void user_task9(char* santams)
+{
+    printf("                                                                                Task 9: Running...|%s\n", santams);
+    uart_puts("                                                                                               |...\n");
+    uart_puts("                                                                                                  |...\n");
+    uart_puts("                                                                                                     |...\n");
+    asm volatile("wfi");
+    task_exit();
+}
+
+
+//將所有task初始化
+void schedule_init()
+{
+    /* enable machine-mode software interrupts. */
+	w_mie(r_mie() | MIE_MSIE);
+    pcb_list = DLL_init();
+    current_task = NULL;
+
+    manage = (pcb_t *)malloc(sizeof(pcb_t));
+	if (manage != NULL) {
+        manage->ID = process_ID;
+        manage->finish = 0;
+        manage->node.next = NULL;
+        manage->node.prev = NULL;
+        manage->priority = 0;
+        manage->context.sp = (reg_t) &(manage->stack[STACK_SIZE - 1]);
+        manage->context.ra = (reg_t) schedule;
+        process_ID++;
+        DLL_add_tail(&manage->node, pcb_list);
+    }
+    task_create(user_task1, "a", 2);
+    task_create(user_task2, "b", 3);
+    task_create(user_task3, "c", 7);
+    task_create(user_task4, "d", 9);
+    task_create(user_task5, "e", 5);
+    task_create(user_task6, "f", 4);
+    task_create(user_task7, "g", 6);
+    task_create(user_task8, "h", 7);
+    task_create(user_task9, "i", 8);
+}
+
+
+
+void schedule()
+{
+    int task_count = DLL_num_nodes(pcb_list);
+    dllNode_t * max = &(manage->node);
+    dllNode_t * target = pcb_list;
+    int temp = 0;
+    while(target->next != NULL)
+    {
+        target = DLL_next_node(target);
+        //找到優先度最高的task，並且必須是沒有被做過的，且沒有在等待中
+        if(((pcb_t *)target)->priority >= temp && ((pcb_t *)target)->finish != 1 && ((pcb_t *)target)->error == 0 && ((pcb_t *)target)->waiting == 0)
+        {
+            //將最優先要做的task編號設給 max 變數
+            temp = ((pcb_t *)target)->priority;
+            max = target;
+        }
+    }
+  
+
+    //如果 ((pcb_t *)max)->priority 為 0 且 ((pcb_t *)max)->finish 也是 0，代表系統目前沒有其他任務要執行
+    if(((pcb_t *)max)->priority == 0 && ((pcb_t *)max)->finish == 0)
+    {
+        current_task = max;
+        
+        target = pcb_list;
+        while(target->next != NULL)
+        {
+            target = DLL_next_node(target);
+            if(((pcb_t *)target)->error == 1 || ((pcb_t *)target)->finish == 1)
+            {
+                DLL_delete(target);
+                free(target);
+            }
+        }
+        
+
+        asm volatile("wfi");
+    }
+    //不是的話代表還有task沒做完，於是將下個 task 的 context 指針設為 max 所代表的task 然後switch_to()
+    else
+    {
+        printf("max = task %d\n\n\n", ((pcb_t *)max)->ID);
+        struct context *next = &(((pcb_t *)max)->context);
+        current_task = max;
+	    switch_to(next, ((pcb_t *)current_task)->pasue_address);        
+    }    
+    
+}
+
+
+
+
+int task_create(void (*start_routin)(char *param), char *param, uint8_t priority)
+{
+    pcb_t * porcess = (pcb_t *)malloc(sizeof(pcb_t));
+	if (porcess != NULL) {
+        porcess->ID = process_ID;
+        porcess->finish = 0;
+        porcess->waiting = 0;
+        porcess->node.next = NULL;
+        porcess->node.prev = NULL;
+        porcess->priority = priority;
+        porcess->pasue_address = (reg_t) start_routin;
+        porcess->context.sp = (reg_t) &(porcess->stack[STACK_SIZE -1]);
+        porcess->context.ra = (reg_t) start_routin;
+        porcess->context.a0 = (reg_t) param;
+        process_ID++;
+        DLL_add_tail(&porcess->node, pcb_list);
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+
+
